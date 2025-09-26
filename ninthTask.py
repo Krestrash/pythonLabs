2d3a728da05b9b0437ac92a83388e93305f04929
krestrash <mark.auhustsenak@gmail.com>
2025-09-26 17:50:32 +0300

    Remade ninth task

--- ninthTask.py
+++ ninthTask.py
@@ -1,6 +1,5 @@
-def valid_ip(ip) -> bool:
-    parts = ip.split(".")
-    return len(parts) == 4 and all(p.isdigit() and 0 <= int(p) <= 255 for p in parts)
+
 
 address = input("Введите IP-адрес: ")
-print("Корректный" if valid_ip(address) else "Некорректный")
+parts = address.split(".")
+print("Корректный" if len(parts) == 4 and all(p.isdigit() and 0 <= int(p) <= 255 for p in parts) else "Некорректный")
