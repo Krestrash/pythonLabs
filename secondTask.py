6685c29b7f6fe414f6a980af9be68b59d58df319
Krestrash <63610701+Krestrash@users.noreply.github.com>
2025-09-26 18:59:30 +0300

    Update secondTask.py

--- secondTask.py
+++ secondTask.py
@@ -1,10 +1,11 @@
 
 str = input("Введите строку: ")
+str= str.lower()
 str = str.replace( "a", '')
 str = str.replace("e",'')
 str = str.replace("i",'')
 str = str.replace("o",'')
 str = str.replace("u",'')
 
-print(str)
+print(f"Строка без гласных:{str}")
 
